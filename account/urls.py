09970ed06f7f039af5792acfcb9ed7464a85a8c6
root <root@pankaj.example.com>
2019-04-24 18:32:53 +0530

    by Pankaj

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import  views
 from django.conf.urls import url
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
 
     url(r'^$', views.Home),
-    url(r'^login/$', login, {'template_name':'account/login.html'}),
+    url(r'^login/$', LoginView.as_view(template_name='account/login.html'),name="login"),
     url(r'^Signup/$', views.register, name='register'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^lab/$', views.lab, name='lab'),
